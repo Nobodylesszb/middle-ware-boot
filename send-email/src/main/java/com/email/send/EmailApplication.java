3e5211f7e478435445589bcd78b48c7967875569
Nobodylesszb <nobodylesszb@163.com>
2020-03-19 20:42:35 +0800

    添加发送邮件模块

---
+++ send-email/src/main/java/com/email/send/EmailApplication.java
@@ -0,0 +1,14 @@
+package com.email.send;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@SpringBootApplication
+@EnableAsync
+public class EmailApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EmailApplication.class,args) ;
+    }
+}

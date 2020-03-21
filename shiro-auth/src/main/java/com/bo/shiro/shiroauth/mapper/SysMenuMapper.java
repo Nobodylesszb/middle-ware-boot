e685d10008953b0183b294cfc41480537169b223
Nobodylesszb <nobodylesszb@163.com>
2020-03-21 19:05:29 +0800

    springboot整合shiro

---
+++ shiro-auth/src/main/java/com/bo/shiro/shiroauth/mapper/SysMenuMapper.java
@@ -0,0 +1,24 @@
+package com.bo.shiro.shiroauth.mapper;
+
+import com.bo.shiro.shiroauth.entity.SysMenuEntity;
+import org.apache.ibatis.annotations.Mapper;
+import java.util.List;
+
+@Mapper
+public interface SysMenuMapper {
+    /**
+     * 根据父菜单，查询子菜单
+     * @param parentId 父菜单ID
+     */
+    List<SysMenuEntity> queryListParentId(Long parentId);
+
+    /**
+     * 查询全部数据
+     */
+    List<SysMenuEntity> selectList() ;
+
+    /**
+     * 获取不包含按钮的菜单列表
+     */
+    List<SysMenuEntity> queryNotButtonList();
+}

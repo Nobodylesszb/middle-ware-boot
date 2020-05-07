a9ddee7af1d006c47c2663d1399a7aebc02dd2ac
Nobodylesszb <nobodylesszb@163.com>
2020-05-07 11:09:01 +0800

    增加spring-aop日志切面

---
+++ springboot-aop/src/main/java/com/bo/springbootaop/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.bo.springbootaop.controller;
+
+import com.bo.springbootaop.common.aop.ControllerWebLog;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Auther: bo
+ * @Date: 2020/5/7 11:05
+ * @Version:
+ * @Description:
+ */
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @GetMapping("/getOne")
+    @ControllerWebLog(name = "查询", intoDb = true)
+    public String getOne(Long id, String name) {
+
+        return "1234";
+    }
+}

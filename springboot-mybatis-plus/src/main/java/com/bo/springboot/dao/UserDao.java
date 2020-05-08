a5519ab8f053ace6d7d2d057d109392acca7bde6
Nobodylesszb <nobodylesszb@163.com>
2020-05-08 22:53:57 +0800

    增加springboot-mybatis-plus模块

---
+++ springboot-mybatis-plus/src/main/java/com/bo/springboot/dao/UserDao.java
@@ -0,0 +1,9 @@
+package com.bo.springboot.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+import com.bo.springboot.model.UserEntity;
+
+public interface UserDao extends BaseMapper<UserEntity>{
+
+}
---
+++ springboot-mybatis-plus/src/main/java/com/bo/springboot/service/UserService.java
@@ -0,0 +1,9 @@
+package com.bo.springboot.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+
+import com.bo.springboot.model.UserEntity;
+
+public interface UserService extends IService<UserEntity>{
+
+}

fcc556f88469fc4f82ee74c24672ed20414f75f4
Nobodylesszb <nobodylesszb@163.com>
2020-09-30 10:55:52 +0800

    对aop实例做了修改

--- springboot-aop/src/main/java/com/bo/springbootaop/common/aop/ControllerWebLog.java
+++ springboot-aop/src/main/java/com/bo/springbootaop/common/aop/ControllerWebLog.java
@@ -10,7 +10,8 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface ControllerWebLog {
-	 String name();//所调用接口的名称
-     boolean intoDb() default false;//标识该条操作日志是否需要持久化存储
- 
+    String name();//所调用接口的名称
+
+    boolean intoDb() default false;//标识该条操作日志是否需要持久化存储
+
 }

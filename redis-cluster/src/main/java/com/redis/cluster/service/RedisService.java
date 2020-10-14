02aabe7299068971354068bb23c570b71e4b8eec
Nobodylesszb <nobodylesszb@163.com>
2020-10-14 14:39:57 +0800

    chore:删除部分模块

--- redis-cluster/src/main/java/com/redis/cluster/service/RedisService.java
+++
@@ -1,5 +0,0 @@
-package com.redis.cluster.service;
-
-public interface RedisService {
-    void saveQueue(String queueKey, String msgBody) ;
-}
--- springboot-muti-source/src/main/java/com/muti/data/source/service/AdminUserService.java
+++
@@ -1,9 +0,0 @@
-package com.muti.data.source.service;
-
-import com.muti.data.source.entity.AdminUser;
-
-public interface AdminUserService {
-
-    AdminUser selectByPrimaryKey(Integer id) ;
-
-}

f6f85231dd255192148885c6fffa571ae3f24669
Nobodylesszb <nobodylesszb@163.com>
2020-10-14 14:50:43 +0800

    chore:删除部分模块

--- elastic-search/src/main/java/com/elastic/search/repository/RequestLogRepository.java
+++
@@ -1,7 +0,0 @@
-package com.elastic.search.repository;
-
-import com.elastic.search.model.RequestLog;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-
-public interface RequestLogRepository extends ElasticsearchRepository<RequestLog,Long> {
-}

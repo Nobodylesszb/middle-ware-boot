a2132644f9e5f057bc98cac67d7e4582e1e9d110
Nobodylesszb <nobodylesszb@163.com>
2020-10-20 11:49:30 +0800

    Feat:添加短信验证sms短信验证码过滤器

---
+++ springboot-security/chapter-four/src/main/java/com/bo/security/validate/smscode/SmsAuthenticationProvider.java
@@ -0,0 +1,42 @@
+package com.bo.security.validate.smscode;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.InternalAuthenticationServiceException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+/**
+ * @auther: bo
+ * @Date: 2020/10/20 11:23
+ * @version: 提供一个认证的provider
+ * @description:
+ */
+@Component
+public class SmsAuthenticationProvider implements AuthenticationProvider {
+    @Autowired
+    private UserDetailService userDetailService;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        SmsAuthenticationToken authenticationToken = (SmsAuthenticationToken) authentication;
+        UserDetails userDetails = userDetailService.loadUserByUsername((String) authenticationToken.getPrincipal());
+
+        if (userDetails == null) {
+            throw new InternalAuthenticationServiceException("未找到与该手机号对应的用户");
+        }
+
+        SmsAuthenticationToken authenticationResult = new SmsAuthenticationToken(userDetails, userDetails.getAuthorities());
+
+        authenticationResult.setDetails(authenticationToken.getDetails());
+
+        return authenticationResult;
+    }
+
+    @Override
+    public boolean supports(Class<?> aClass) {
+        return SmsAuthenticationToken.class.isAssignableFrom(aClass);
+    }
+}

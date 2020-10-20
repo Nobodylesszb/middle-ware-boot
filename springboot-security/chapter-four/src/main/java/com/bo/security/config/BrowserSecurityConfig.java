1605f0492aa9c06274b21a175380ea64a93b2d73
Nobodylesszb <nobodylesszb@163.com>
2020-10-20 15:29:59 +0800

    Feat:添加sms认证页面

---
+++ springboot-security/chapter-four/src/main/java/com/bo/security/config/BrowserSecurityConfig.java
@@ -0,0 +1,66 @@
+package com.bo.security.config;
+
+
+import com.bo.security.security.CustomAuthenticationFailureHandler;
+import com.bo.security.security.CustomAuthenticationSuccessHandler;
+import com.bo.security.validate.imagecode.ValidateCodeFilter;
+import com.bo.security.validate.smscode.SmsAuthenticationConfig;
+import com.bo.security.validate.smscode.SmsCodeFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private CustomAuthenticationSuccessHandler authenticationSucessHandler;
+
+    @Autowired
+    private CustomAuthenticationFailureHandler authenticationFailureHandler;
+
+    @Autowired
+    private ValidateCodeFilter validateCodeFilter;
+
+    @Autowired
+    private SmsCodeFilter smsCodeFilter;
+
+    @Autowired
+    private SmsAuthenticationConfig smsAuthenticationConfig;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.addFilterBefore(validateCodeFilter, UsernamePasswordAuthenticationFilter.class) // 添加验证码校验过滤器
+                .addFilterBefore(smsCodeFilter, UsernamePasswordAuthenticationFilter.class) // 添加短信验证码校验过滤器
+                .formLogin() // 表单登录
+                // http.httpBasic() // HTTP Basic
+                .loginPage("/authentication/require") // 登录跳转 URL
+                .loginProcessingUrl("/login") // 处理表单登录 URL
+                .successHandler(authenticationSucessHandler) // 处理登录成功
+                .failureHandler(authenticationFailureHandler) // 处理登录失败
+                .and()
+                .authorizeRequests() // 授权配置
+                .antMatchers("/authentication/require",
+                        "/login.html", "/code/image", "/code/sms").permitAll() // 无需认证的请求路径
+                .anyRequest()  // 所有请求
+                .authenticated() // 都需要认证
+                .and()
+                .csrf().disable()
+                .apply(smsAuthenticationConfig); // 将短信验证码认证配置加到 Spring Security 中
+    }
+}
--- springboot-security/chapter-four/src/main/java/com/bo/security/config/WebSecurityConfig.java
+++ springboot-security/chapter-four/src/main/java/com/bo/security/config/WebSecurityConfig.java
@@ -1,111 +1,111 @@
-package com.bo.security.config;
-
-import com.bo.security.security.*;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.session.SessionRegistry;
-import org.springframework.security.core.session.SessionRegistryImpl;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-/**
- * @auther: bo
- * @Date: 2020/10/13 16:28
- * @version:
- * @description:
- */
-@Configuration
-@SuppressWarnings("all")
-@EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Autowired
-    private CustomUserDetailsService userDetailsService;
-
-    @Autowired
-    private CustomAuthenticationSuccessHandler customAuthenticationSuccessHandler;
-    @Autowired
-    private CustomAuthenticationFailureHandler customAuthenticationFailureHandler;
-    @Autowired
-    private CustomLogoutSuccessHandler logoutSuccessHandler;
-
+//package com.bo.security.config;
+//
+//import com.bo.security.security.*;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.builders.WebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.core.session.SessionRegistry;
+//import org.springframework.security.core.session.SessionRegistryImpl;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//
+///**
+// * @auther: bo
+// * @Date: 2020/10/13 16:28
+// * @version:
+// * @description:
+// */
+//@Configuration
+//@SuppressWarnings("all")
+//@EnableWebSecurity
+//@EnableGlobalMethodSecurity(prePostEnabled = true)
+//public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//    @Autowired
+//    private CustomUserDetailsService userDetailsService;
+//
+//    @Autowired
+//    private CustomAuthenticationSuccessHandler customAuthenticationSuccessHandler;
+//    @Autowired
+//    private CustomAuthenticationFailureHandler customAuthenticationFailureHandler;
+//    @Autowired
+//    private CustomLogoutSuccessHandler logoutSuccessHandler;
+//
+////    @Override
+////    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+////        auth.userDetailsService(userDetailsService).passwordEncoder(new PasswordEncoder() {
+////            @Override
+////            public String encode(CharSequence charSequence) {
+////                return charSequence.toString();
+////            }
+////
+////            @Override
+////            public boolean matches(CharSequence charSequence, String s) {
+////                return s.equals(charSequence.toString());
+////            }
+////        });
+////    }
+//
+//    /**
+//     * 使用内置的加密方式，注意，储存在数据库的密码必须和此加密方式一致
+//     * @param auth
+//     * @throws Exception
+//     */
 //    @Override
 //    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.userDetailsService(userDetailsService).passwordEncoder(new PasswordEncoder() {
-//            @Override
-//            public String encode(CharSequence charSequence) {
-//                return charSequence.toString();
-//            }
-//
-//            @Override
-//            public boolean matches(CharSequence charSequence, String s) {
-//                return s.equals(charSequence.toString());
-//            }
-//        });
+//        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
 //    }
-
-    /**
-     * 使用内置的加密方式，注意，储存在数据库的密码必须和此加密方式一致
-     * @param auth
-     * @throws Exception
-     */
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
-    }
-
-    //密码编码器
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public SessionRegistry sessionRegistry() {
-        return new SessionRegistryImpl();
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                // 如果有允许匿名的url，填在下面
-                .antMatchers("/login/invalid").permitAll()
-                .anyRequest().authenticated().and()
-                // 设置登陆页
-                .formLogin().loginPage("/login")
-                .successHandler(customAuthenticationSuccessHandler)
-                .failureHandler(customAuthenticationFailureHandler)
-//                .failureUrl("/login/error")
-//                .defaultSuccessUrl("/")
-                .permitAll().and()
-                .logout()
-                .logoutUrl("/logout")
-                .logoutSuccessHandler(logoutSuccessHandler)
-                .deleteCookies("JSESSIONID").and()
-                .sessionManagement()
-                .invalidSessionUrl("/login/invalid")
-                .maximumSessions(1)
-                // 当达到最大值时，是否保留已经登录的用户
-                .maxSessionsPreventsLogin(false)
-                // 当达到最大值时，旧用户被踢出后的操作
-                .expiredSessionStrategy(new CustomExpiredSessionStrategy())
-                .sessionRegistry(sessionRegistry());
-
-        // 关闭CSRF跨域
-        http.csrf().disable();
-    }
-
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        // 设置拦截忽略文件夹，可以对静态资源放行
-        web.ignoring().antMatchers("/css/**", "/js/**");
-    }
-}
+//
+//    //密码编码器
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//
+//    @Bean
+//    public SessionRegistry sessionRegistry() {
+//        return new SessionRegistryImpl();
+//    }
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.authorizeRequests()
+//                // 如果有允许匿名的url，填在下面
+//                .antMatchers("/login/invalid").permitAll()
+//                .anyRequest().authenticated().and()
+//                // 设置登陆页
+//                .formLogin().loginPage("/login")
+//                .successHandler(customAuthenticationSuccessHandler)
+//                .failureHandler(customAuthenticationFailureHandler)
+////                .failureUrl("/login/error")
+////                .defaultSuccessUrl("/")
+//                .permitAll().and()
+//                .logout()
+//                .logoutUrl("/logout")
+//                .logoutSuccessHandler(logoutSuccessHandler)
+//                .deleteCookies("JSESSIONID").and()
+//                .sessionManagement()
+//                .invalidSessionUrl("/login/invalid")
+//                .maximumSessions(1)
+//                // 当达到最大值时，是否保留已经登录的用户
+//                .maxSessionsPreventsLogin(false)
+//                // 当达到最大值时，旧用户被踢出后的操作
+//                .expiredSessionStrategy(new CustomExpiredSessionStrategy())
+//                .sessionRegistry(sessionRegistry());
+//
+//        // 关闭CSRF跨域
+//        http.csrf().disable();
+//    }
+//
+//    @Override
+//    public void configure(WebSecurity web) throws Exception {
+//        // 设置拦截忽略文件夹，可以对静态资源放行
+//        web.ignoring().antMatchers("/css/**", "/js/**");
+//    }
+//}

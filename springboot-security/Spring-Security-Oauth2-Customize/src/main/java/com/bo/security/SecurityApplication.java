be4b76cc903757963b210ff659304f3857d2a8e4
Nobodylesszb <nobodylesszb@163.com>
2020-10-21 17:51:04 +0800

    feat:增加oauth2整合短信登录

---
+++ springboot-security/Spring-Security-Oauth2-Customize/src/main/java/com/bo/security/SecurityApplication.java
@@ -0,0 +1,18 @@
+package com.bo.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @auther: bo
+ * @Date: 2020/10/21 17:43
+ * @version:
+ * @description:
+ */
+
+@SpringBootApplication
+public class SecurityApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityApplication.class, args);
+    }
+}

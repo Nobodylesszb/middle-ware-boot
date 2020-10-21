12958873056a599a3b89a9ef796779ca66b67a20
Nobodylesszb <nobodylesszb@163.com>
2020-10-21 15:09:16 +0800

    feat:增加oauth2的基本设置

---
+++ springboot-security/Spring-Security-Oauth2-Guide/src/main/java/com/bo/security/SecurityApplication.java
@@ -0,0 +1,15 @@
+package com.bo.security;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * @auther: bo
+ * @Date: 2020/10/21 15:04
+ * @version:
+ * @description:
+ */
+public class SecurityApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityApplication.class, args);
+    }
+}

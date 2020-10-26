7cc2c50c01e556f23cabdfa235140a4e7e3bd87f
Nobodylesszb <nobodylesszb@163.com>
2020-10-26 15:32:29 +0800

    Feat:新增kafka起始架构

---
+++ springboot-kafka/springboot-kafka-starter/src/main/java/com/bo/springboot/KafkaApplication.java
@@ -0,0 +1,17 @@
+package com.bo.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @auther: bo
+ * @Date: 2020/10/26 15:31
+ * @version:
+ * @description:
+ */
+@SpringBootApplication
+public class KafkaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaApplication.class, args);
+    }
+}

665707362ebb6ad7c57059bc6037a35a55312997
Nobodylesszb <nobodylesszb@163.com>
2020-10-27 10:22:58 +0800

    Feat:新增springCache

---
+++ springboot-cache/spring-cache-starter/src/main/java/com/bo/springboot/Application.java
@@ -0,0 +1,18 @@
+package com.bo.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @auther: bo
+ * @Date: 2020/10/27 10:16
+ * @version:
+ * @description:
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}

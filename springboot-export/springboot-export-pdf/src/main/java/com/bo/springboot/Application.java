06513501344d206e697c34f14611812298e11281
Nobodylesszb <nobodylesszb@163.com>
2020-10-28 14:56:51 +0800

    Feat:新增导出部分架构

---
+++ springboot-export/springboot-export-pdf/src/main/java/com/bo/springboot/Application.java
@@ -0,0 +1,17 @@
+package com.bo.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @auther: bo
+ * @Date: 2020/10/28 14:52
+ * @version:
+ * @description:
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}

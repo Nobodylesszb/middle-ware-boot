4ba8b386f307aeeb958883ac17c52fc5c9a87d91
Nobodylesszb <nobodylesszb@163.com>
2020-10-28 09:35:28 +0800

    Feat:开启缓存功能

--- springboot-cache/spring-cache-starter/src/main/java/com/bo/springboot/Application.java
+++ springboot-cache/spring-cache-starter/src/main/java/com/bo/springboot/Application.java
@@ -2,6 +2,7 @@ package com.bo.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 /**
  * @auther: bo
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @description:
  */
 @SpringBootApplication
+@EnableCaching
 public class Application {
 
     public static void main(String[] args) {

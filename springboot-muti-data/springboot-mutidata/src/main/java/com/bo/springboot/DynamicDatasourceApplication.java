98060564871e3b5cc008784d29670ea0e46c8a05
Nobodylesszb <nobodylesszb@163.com>
2020-10-29 16:56:50 +0800

    Feat:新增springboot集成多数据源

---
+++ springboot-muti-data/springboot-mutidata/src/main/java/com/bo/springboot/DynamicDatasourceApplication.java
@@ -0,0 +1,18 @@
+package com.bo.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @auther: bo
+ * @Date: 2020/10/29 16:56
+ * @version:
+ * @description:
+ */
+@SpringBootApplication
+public class DynamicDatasourceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DynamicDatasourceApplication.class, args);
+    }
+
+}

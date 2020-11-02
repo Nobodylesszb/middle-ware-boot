897616958566a0a993240351ef56663b4d1d5751
Nobodylesszb <nobodylesszb@163.com>
2020-11-02 18:46:23 +0800

    feat:添加参数校验validate

---
+++ springboot-admin/src/main/java/com/bo/springboot/admin/module/sys/controller/AbstractController.java
@@ -0,0 +1,22 @@
+package com.bo.springboot.admin.module.sys.controller;
+
+import com.bo.springboot.admin.module.sys.entity.SysUserEntity;
+import org.apache.shiro.SecurityUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Controller公共组件
+ * bo
+ */
+public abstract class AbstractController {
+	protected Logger logger = LoggerFactory.getLogger(getClass());
+	
+	protected SysUserEntity getUser() {
+		return (SysUserEntity) SecurityUtils.getSubject().getPrincipal();
+	}
+
+	protected Long getUserId() {
+		return getUser().getUserId();
+	}
+}

a658e6f6038f194d1b78a5d6441b331c74be1165
Nobodylesszb <nobodylesszb@163.com>
2020-11-03 18:17:09 +0800

    feat:设置跨域

---
+++ springboot-admin/src/main/java/com/bo/springboot/admin/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.bo.springboot.admin.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @auther: bo
+ * @Date: 2020/11/3 18:12
+ * @version:
+ * @description:
+ */
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowCredentials(true)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .maxAge(3600);
+    }
+}

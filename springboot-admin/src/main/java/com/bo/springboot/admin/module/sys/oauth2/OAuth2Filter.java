aa4dc8d09431661bd48b75fdc1d7cb924af10239
Nobodylesszb <nobodylesszb@163.com>
2020-11-03 16:28:28 +0800

    feat:增加shiro对jwt解析过滤器

---
+++ springboot-admin/src/main/java/com/bo/springboot/admin/module/sys/oauth2/OAuth2Filter.java
@@ -0,0 +1,87 @@
+package com.bo.springboot.admin.module.sys.oauth2;
+
+import com.bo.springboot.admin.common.utils.HttpContextUtils;
+import com.bo.springboot.admin.common.utils.R;
+import com.google.gson.Gson;
+import org.apache.commons.lang.StringUtils;
+import org.apache.http.HttpStatus;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.web.filter.authc.AuthenticatingFilter;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @auther: bo
+ * @Date: 2020/11/3 15:58
+ * @version:
+ * @description:
+ */
+public class OAuth2Filter extends AuthenticatingFilter {
+    @Override
+    protected AuthenticationToken createToken(ServletRequest request, ServletResponse response) throws Exception {
+        //获取请求token
+        String token = getRequestToken((HttpServletRequest) request);
+
+        if(StringUtils.isBlank(token)){
+            return null;
+        }
+
+        return new OAuth2Token(token);
+    }
+
+    private String getRequestToken(HttpServletRequest httpRequest) {
+        //从header中获取token
+        String token = httpRequest.getHeader("token");
+
+        //如果header中不存在token，则从参数中获取token
+        if(StringUtils.isBlank(token)){
+            token = httpRequest.getParameter("token");
+        }
+        return token;
+    }
+
+    @Override
+    protected boolean isAccessAllowed(ServletRequest request, ServletResponse response, Object mappedValue) {
+        if(((HttpServletRequest) request).getMethod().equals(RequestMethod.OPTIONS.name())){
+            return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    protected boolean onAccessDenied(ServletRequest request, ServletResponse response) throws Exception {
+        String token = getRequestToken((HttpServletRequest) request);
+        if(StringUtils.isBlank(token)){
+            HttpServletResponse httpResponse = (HttpServletResponse) response;
+            httpResponse.setHeader("Access-Control-Allow-Credentials", "true");
+            httpResponse.setHeader("Access-Control-Allow-Origin", HttpContextUtils.getOrigin());
+
+            String json = new Gson().toJson(R.error(HttpStatus.SC_UNAUTHORIZED, "invalid token"));
+
+            httpResponse.getWriter().print(json);
+
+            return false;
+        }
+
+        return executeLogin(request, response);
+    }
+
+    /**
+     * 执行登陆操作
+     */
+    @Override
+    protected boolean executeLogin(ServletRequest request, ServletResponse response) throws Exception {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        String token = getRequestToken(httpServletRequest);
+        OAuth2Token jwtToken = new OAuth2Token(token);
+        // 提交给realm进行登入，如果错误他会抛出异常并被捕获
+        getSubject(request, response).login(jwtToken);
+        // 如果没有抛出异常则代表登入成功，返回true
+        return true;
+    }
+}

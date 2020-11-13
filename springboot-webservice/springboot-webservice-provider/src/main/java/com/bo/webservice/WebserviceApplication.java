ceacac8cfeec3b24aa9eb303b7bc5a96f6d08cad
Nobodylesszb <nobodylesszb@163.com>
2020-11-13 14:01:23 +0800

    feat:增加webservice基本框架

---
+++ springboot-webservice/springboot-webservice-provider/src/main/java/com/bo/webservice/WebserviceApplication.java
@@ -0,0 +1,17 @@
+package com.bo.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @auther: bo
+ * @Date: 2020/11/13 13:59
+ * @version:
+ * @description:
+ */
+@SpringBootApplication
+public class WebserviceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebserviceApplication.class, args);
+    }
+}

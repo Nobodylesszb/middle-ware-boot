09d678135f99b706240630f2bcfbeec65c32883d
Nobodylesszb <nobodylesszb@163.com>
2020-11-16 17:23:36 +0800

    feat:增加动态配置数据源配置

---
+++ springboot-admin/src/main/java/com/bo/springboot/admin/datasource/config/DynamicDataSourceConfig.java
@@ -0,0 +1,62 @@
+package com.bo.springboot.admin.datasource.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.bo.springboot.admin.datasource.properties.DataSourceProperties;
+import com.bo.springboot.admin.datasource.properties.DynamicDataSourceProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @auther: bo
+ * @Date: 2020/11/16 17:07
+ * @version: 配置多数据源
+ * @description:
+ */
+@Configuration
+@EnableConfigurationProperties(DynamicDataSourceProperties.class)
+public class DynamicDataSourceConfig {
+    @Autowired
+    private DynamicDataSourceProperties properties;
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource.druid")
+    public DataSourceProperties dataSourceProperties() {
+        return new DataSourceProperties();
+    }
+
+
+    @Bean
+    public DynamicDataSource dynamicDataSource(DataSourceProperties dataSourceProperties) {
+        //设置动态的
+        DynamicDataSource dynamicDataSource = new DynamicDataSource();
+        dynamicDataSource.setTargetDataSources(getDynamicDataSource());
+
+        //默认数据源
+        DruidDataSource defaultDataSource = DynamicDataSourceFactory.buildDruidDataSource(dataSourceProperties);
+        dynamicDataSource.setDefaultTargetDataSource(defaultDataSource);
+
+        return dynamicDataSource;
+    }
+
+    /**
+     * 过得dataSourceMap
+     * @return
+     */
+    private Map<Object, Object> getDynamicDataSource(){
+        Map<String, DataSourceProperties> dataSourcePropertiesMap = properties.getDatasource();
+        Map<Object, Object> targetDataSources = new HashMap<>(dataSourcePropertiesMap.size());
+        dataSourcePropertiesMap.forEach((k, v) -> {
+            DruidDataSource druidDataSource = DynamicDataSourceFactory.buildDruidDataSource(v);
+            targetDataSources.put(k, druidDataSource);
+        });
+
+        return targetDataSources;
+    }
+
+}

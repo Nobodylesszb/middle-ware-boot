06a0a47b2a7cedd576ca59ec755b6723b3bfd054
Nobodylesszb <nobodylesszb@163.com>
2020-11-17 11:40:20 +0800

    fea:新增mybatis配置

---
+++ springboot-mybatis/src/main/java/com/bo/springboot/MybatisApplication.java
@@ -0,0 +1,17 @@
+package com.bo.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @auther: bo
+ * @Date: 2020/11/17 11:29
+ * @version:
+ * @description:
+ */
+@SpringBootApplication
+public class MybatisApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisApplication.class, args);
+    }
+}

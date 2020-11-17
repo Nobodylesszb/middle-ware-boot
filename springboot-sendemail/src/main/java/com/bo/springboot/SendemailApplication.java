8a2845305c9e8185f098a0e78bfd1677a1efa7b7
Nobodylesszb <nobodylesszb@163.com>
2020-11-17 18:36:08 +0800

    feat:增加发送邮件

---
+++ springboot-sendemail/src/main/java/com/bo/springboot/SendemailApplication.java
@@ -0,0 +1,17 @@
+package com.bo.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @auther: bo
+ * @Date: 2020/11/17 18:21
+ * @version:
+ * @description:
+ */
+@SpringBootApplication
+public class SendemailApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SendemailApplication.class, args);
+    }
+}

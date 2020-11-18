a5d214c936fc928f92eea930c6f0b8956cb7fe68
Nobodylesszb <nobodylesszb@163.com>
2020-11-18 14:18:44 +0800

    feat:增加自定义controller

---
+++ springboot-throwException/src/main/java/com/bo/springboot/ThrowableApplication.java
@@ -0,0 +1,15 @@
+package com.bo.springboot;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * @auther: bo
+ * @Date: 2020/11/18 14:12
+ * @version:
+ * @description:
+ */
+public class ThrowableApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ThrowableApplication.class, args);
+    }
+}

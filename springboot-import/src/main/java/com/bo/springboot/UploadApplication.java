59e3aa79d4baa25216564cb53d285904356e9806
Nobodylesszb <nobodylesszb@163.com>
2020-11-19 14:43:39 +0800

    feat:文件上传基础搭建

---
+++ springboot-import/src/main/java/com/bo/springboot/UploadApplication.java
@@ -0,0 +1,17 @@
+package com.bo.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @auther: bo
+ * @Date: 2020/11/19 14:41
+ * @version:
+ * @description:
+ */
+@SpringBootApplication
+public class UploadApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UploadApplication.class, args);
+    }
+}

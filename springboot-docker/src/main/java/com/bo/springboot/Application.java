e50512492ab76f5983cead08198c3b5ee92a72e4
Nobodylesszb <nobodylesszb@163.com>
2020-11-20 15:24:21 +0800

    feat:新增docker项目

---
+++ springboot-docker/src/main/java/com/bo/springboot/Application.java
@@ -0,0 +1,15 @@
+package com.bo.springboot;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * @auther: bo
+ * @Date: 2020/11/20 15:06
+ * @version:
+ * @description:
+ */
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+}

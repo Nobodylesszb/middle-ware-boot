a51438378717b4d3f8300beae600563b70e60bc9
Nobodylesszb <nobodylesszb@163.com>
2021-03-31 19:47:10 +0800

    feat:增加工具类项目

---
+++ springboot-utils/src/main/java/com/bo/springboot/UtilsApplication.java
@@ -0,0 +1,17 @@
+package com.bo.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @auther: bo
+ * @Date: 2021/3/31 19:43
+ * @version:
+ * @description:
+ */
+@SpringBootApplication
+public class UtilsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UtilsApplication.class, args);
+    }
+}

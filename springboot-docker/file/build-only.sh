9e1a2516ee9a029648d69329c27d820dfe1efc57
Nobodylesszb <nobodylesszb@163.com>
2021-06-04 14:31:18 +0800

    feat:使用dockerfile maven-plugin 打包镜像

---
+++ springboot-docker/file/build-only.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cd `dirname $0`
+
+img_mvn="maven:3.3.3-jdk-8"                 # docker image of maven
+m2_cache=~/.m2                              # the local maven cache dir
+proj_home=$PWD                              # the project root dir
+img_output="deepexi-daas-model:v1.0"      # output image tag
+
+git pull  # should use git clone https://name:pwd@xxx.git
+
+echo "use docker maven"
+docker run --rm \
+   -v $m2_cache:/root/.m2 \
+   -v $proj_home:/usr/src/mymaven \
+   -w /usr/src/mymaven $img_mvn mvn clean package -Dmaven.test.skip=true -U
+
+sudo mv $proj_home/deepexi-daas-model-provider/target/deepexi-daas-model-*.jar $proj_home/deepexi-daas-model-provider/target/demo.jar # 兼容所有sh脚本
+
+echo "构建镜像"
+docker build -t $img_output .
